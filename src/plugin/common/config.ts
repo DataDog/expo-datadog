8115ccf374234a0193407bb398cd83266730206a
Marco Saia <marco.saia@datadoghq.com>
2024-06-13 15:36:57 +0200

    Added missing copyright headers

--- src/plugin/common/config.ts
+++ src/plugin/common/config.ts
@@ -1,2 +1,8 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2016-Present Datadog, Inc.
+ */
+
 export const DEFAULT_DATADOG_GRADLE_PLUGIN_VERSION = "1.14.0";
 export const IOS_DSYMS_BUILD_PHASE_NAME = "Upload dSYMs to Datadog";
